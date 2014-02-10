58ee1c70512a0b4a43ef211f9ef82fdbabd1361e
ashley williams <heyashleyashley@gmail.com>
2014-02-10 09:50:59 -0500

    step6: - move application to app.js - create spacecatControllers module - define 2 controllers: list and detail - link details to index and detail partials in app - add individual cat json - build detail partial - add angular routes resource - stylez for detail

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,19 @@
 'use strict';
 
-var spacecatApp = angular.module('spacecatApp', []);
+var spacecatControllers = angular.module('spacecatControllers', []);
 
-spacecatApp.controller('CatListCtrl', function($scope, $http){
-  $http.get('cats/cats.json').success(function(data) {
-    $scope.cats = data;
-  });
+spacecatApp.controller('CatListCtrl', ['$scope', '$http',
+  function($scope, $http){
+    $http.get('cats/cats.json').success(function(data) {
+      $scope.cats = data;
+    });
  
-  $scope.orderProp = 'name';
-});
+    $scope.orderProp = 'name';
+  }]);
+
+spacecatControllers.controller('CatDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get("cats/cats-" + $routeParams.catId + '.json').success(function(data) {
+      $scope.cat = data[0];
+    });
+  }]);
